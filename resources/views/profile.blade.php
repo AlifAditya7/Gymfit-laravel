50068f09a551ad40ad5229b900ed2f0d6c1184c4
Faisal Rizky <faisalrizki35@gmail.com>
2024-02-22 20:49:28 +0700

    pepek

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -144,7 +144,7 @@
                 <li><a href="./team.html">Profile</a></li>
                 <li><a href="#">Other</a>
                     <ul class="dropdown">
-                        <li><a href="{{ url('/bmi') }}">BMI Calculate</a></li>
+                        <li><a href="#">BMI Calculate</a></li>
                         <li><a href="{{ route('login') }}">Log In</a></li>
                         <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 {{ __('Log out') }}
@@ -235,7 +235,9 @@
                 />
             </div>
             <div class="text-center mt-3">
-                <p id="selamatDatang">Selamat datang</p>
+                @auth
+                    <p id="selamatDatang">Selamat datang, {{ Auth::user()->name }}</p>
+                @endauth
                 <!-- Tambahkan input type file untuk mengganti gambar -->
                 <input
                     type="file"
