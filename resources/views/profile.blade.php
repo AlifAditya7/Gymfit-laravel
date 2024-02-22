f96ecaa93933b1fbbcf3c19f105e6756347a0127
kirinosuki <Septian070903@gmail.com>
2024-02-22 20:41:57 +0700

    bmi

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -131,7 +131,7 @@
                 <li><a href="./team.html">Profile</a></li>
                 <li><a href="#">Other</a>
                     <ul class="dropdown">
-                        <li><a href="#">BMI Calculate</a></li>
+                        <li><a href="{{ route('/bmi') }}">BMI Calculate</a></li>
                         <li><a href="{{ route('login') }}">Log In</a></li>
                         <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 {{ __('Log out') }}
